e5b64e0ca3af7d9b3a1a8727bedf0a3583803b59
zsh <13037290702@tax.com>
2024-10-28 18:41:10 +0800

    fd

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -11,10 +11,13 @@
                     <h3 text="2xl" justify="center">{{ item }}</h3>
                     </el-carousel-item>
                 </el-carousel>
+                <!-- tag -->
+                <el-scrollbar height="60px">
+                    <el-tabs v-model="activeName" v-for="(item,index) in gameType" :key="index" class="demo-tabs" @tab-click="handleClick">
+                        <el-tab-pane label="item.name" name="item.code">User</el-tab-pane>
+                    </el-tabs>
+                </el-scrollbar>
 
-                <div style="width:100%; height:3000px;">
-
-                </div>
             </el-scrollbar>
         </el-main>
        
@@ -24,8 +27,32 @@
 
 
 <script setup>
-    import {ref} from 'vue'
+    import {ref,onMounted} from 'vue'
     import MyHeader from '../home/MyHeader.vue'
+    import {service} from '@/components/js/http.js';
+
+    let gameType = ref([])
+
+    onMounted(()=>{
+        getGameType()
+    })
+
+    function getGameType(){
+        service.get('/api/game/type').then(res=>{
+            let re = res.data
+            if(re.code === 200){
+
+                gameType = re.data
+            }
+
+        })
+    }
+
+    // 重写为箭头函数形式的 handleClick
+    const handleClick = (tab, event) => {
+      console.log(tab);
+      console.log(event);
+    };
 </script>
 
 <style>
@@ -40,5 +67,11 @@
         background-color: #ecf5ff;
         /* height: 94%; */
         overflow: auto;
+    }
+    .demo-tabs > .el-tabs__content {
+        padding: 32px;
+        color: #6b778c;
+        font-size: 32px;
+        font-weight: 600;
     }
 </style>
