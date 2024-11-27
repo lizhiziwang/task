1db2a9cf901911839993022ebeec808087a2bc4c
zsh <13037290702@tax.com>
2024-11-27 17:58:36 +0800

    123

--- src/components/js/layer.js
+++ src/components/js/layer.js
@@ -1,64 +1,94 @@
-import { XYZ } from 'ol/source';
+import { XYZ,TileImage } from 'ol/source';
 import TileLayer from 'ol/layer/Tile';
 import { TileWMS } from 'ol/source';
-import LayerGroup from 'ol/layer/Group';
-import tk from '@/gis/token';
+import TileGrid from 'ol/tilegrid/TileGrid'
 
-const tdtLayer_img = new TileLayer({
+
+const gaode = new TileLayer({
+    visible: true,
     source: new XYZ({
-        url: `http://t{0-7}.tianditu.com/DataServer?T=img_w&tk=${tk}&x={x}&y={y}&l={z}`,
+        visible: true,
+        // 高德影像地图
+        // url: 'https://webst01.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
+        // 高德矢量地图
+        url: 'http://webst0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=2&style=7&key=17be85abdc35ac0635cfcfe31fe10936',
+        // 设置canvas滤镜 黑色
+        tileLoadFunction: function (imageTile, src) {
+            let img = new Image();
+            // 设置图片不从缓存取，从缓存取可能会出现跨域，导致加载失败
+            img.setAttribute("crossOrigin", "anonymous");
+            img.onload = function () {
+                let canvas = document.createElement("canvas");
+                let w = img.width;
+                let h = img.height;
+                canvas.width = w;
+                canvas.height = h;
+                let context = canvas.getContext("2d");
+
+                context.filter =
+                "grayscale(0%) invert(100%) sepia(10%) hue-rotate(175deg) saturate(100%) brightness(100%) contrast(120%)";
+                // grayscale 灰度   invert反相   sepia将图像转化成深褐色  saturate饱和度   brightness暗度 contrast对比度
+
+                context.drawImage(img, 0, 0, w, h, 0, 0, w, h);
+                imageTile.getImage().src = canvas.toDataURL("image/png");
+            };
+            img.src = src;
+        },
+    })
+});
+const geode_yx = new TileLayer({
+    visible: true,
+    source: new XYZ({
+        visible: true,
+        // 高德影像地图
+        // url: 'https://webst01.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
+        // 高德矢量地图
+        url: 'http://webst0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=2&style=6&key=17be85abdc35ac0635cfcfe31fe10936',
+        // 设置canvas滤镜 黑色
+    })
+});
+const tianditu = new TileLayer({
+    source: new XYZ({
+        url: `http://t1.tianditu.com/DataServer?T=vec_w&tk=f0ef2118b8ccd76bfd9acc8217e5dab0&x={x}&y={y}&l={z}`,
     })
 });
 
-const tdtLayer_cia = new TileLayer({
-    source: new XYZ({
-        url: `http://t{0-7}.tianditu.com/DataServer?T=cia_w&x={x}&y={y}&l={z}&tk=${tk}`,
-    })
+var resolutions = [];
+for (var i = 0; i < 19; i++) {
+    resolutions[i] = Math.pow(2, 18 - i);
+}
+
+const baidu = new TileLayer({
+    source:new TileImage(
+        {
+            tileGrid: new TileGrid({
+                origin: [0, 0],
+                resolutions: resolutions
+            }),
+            projection:"EPSG:3857",
+        
+            tileUrlFunction: function (tileCoord) {
+                if (!tileCoord) {
+                    return "";
+                }
+                var z = tileCoord[0];
+                var x = tileCoord[1];
+                var y = tileCoord[2];
+        
+                x = x<0?x:'M'+(-x);
+                y = -y
+                return "http://online3.map.bdimg.com/onlinelabel/?qt=tile&x=" + x + "&y=" + y + "&z=" + z + "&styles=pl&udt=20151021&scaler=1&p=1";
+            }
+        }
+    )
 });
 
-const tdtLayer_ibo = new TileLayer({
-    source: new XYZ({
-        url: `http://t{0-7}.tianditu.com/DataServer?T=ibo_w&x={x}&y={y}&l={z}&tk=${tk}`
-    })
-});
 
-const tdtLayer_vec = new TileLayer({
-    source: new XYZ({
-        url: `http://t{0-7}.tianditu.com/DataServer?T=vec_w&x={x}&y={y}&l={z}&tk=${tk}`
-    })
-});
-const tdtLayer_cva = new TileLayer({
-    source: new XYZ({
-        url: `http://t{0-7}.tianditu.com/DataServer?T=cva_w&x={x}&y={y}&l={z}&tk=${tk}`,
-    })
-});
-//天地图影像图
-const tdtImgLayer = new LayerGroup({
-    layers: [
-        tdtLayer_img,
-        tdtLayer_ibo,
-        tdtLayer_cia,
-    ]
-})
-//天地图矢量图
-const tdtVectorLayer = new LayerGroup({
-    layers: [
-        tdtLayer_vec,
-        tdtLayer_cva,
-    ]
-});
-//WMS服务图层（农田图层）
-const dFarmlandSource = new TileWMS({
-    url: 'http://localhost:8080/geoserver/cite/wms',
-    params: {
-      'LAYERS': 'cite:胡_result',
-      'VERSION': '1.1.0',
-      'TILED': true
-    },
-    serverType: 'geoserver'
-  });
-  const dFarmlandLayer = new TileLayer({
-    //ratio: 1,
-    source: dFarmlandSource
-  });
-export { tdtImgLayer, tdtVectorLayer, dFarmlandLayer};
+const maps = {
+    "高德地图":gaode,
+    "高德影像":geode_yx,
+    "天地图":tianditu,
+    "百度地图":baidu
+}
+
+export {maps};
